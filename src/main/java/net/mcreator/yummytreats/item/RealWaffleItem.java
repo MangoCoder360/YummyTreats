36fd7400933246a41f2ec1ea6728d8b07f10c14d
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-10-30 12:47:50 -0700

    Initial commit

---
+++ src/main/java/net/mcreator/yummytreats/item/RealWaffleItem.java
@@ -0,0 +1,46 @@
+
+package net.mcreator.yummytreats.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.yummytreats.YummyTreatsModElements;
+
+@YummyTreatsModElements.ModElement.Tag
+public class RealWaffleItem extends YummyTreatsModElements.ModElement {
+	@ObjectHolder("yummy_treats:real_waffle")
+	public static final Item block = null;
+	public RealWaffleItem(YummyTreatsModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
+			setRegistryName("real_waffle");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
